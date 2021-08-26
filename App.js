934a7e36f8157f138c407433a62afa30ce2f1df6
Hwajiin <borderline0px@gmail.com>
2021-08-26 19:21:27 +0900

    stack, tab navigation

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import Stack from "./Navigation/Stack";
 
 const cacheImages = (images) =>
   images.map((image) => {
@@ -12,19 +16,24 @@ const cacheImages = (images) =>
     }
   });
 
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = cacheImages([
       "https://images.unsplash.com/photo-1629872874038-b1d600221640?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
       require("./assets/splash.png"),
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
 
   return isReady ? (
-    <Text>Ready</Text>
+    <NavigationContainer>
+      <Stack />
+    </NavigationContainer>
   ) : (
     <AppLoading
       startAsync={loadAssets}
